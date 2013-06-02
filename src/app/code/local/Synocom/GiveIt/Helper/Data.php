47ad10d29354f326b01cc4a57b91ee24bad182a5
Brennon Blokland <b.blokland@drecomm.nl>
2013-06-03 01:38:41 +0200

    [TASK] Add phpDoc

--- src/app/code/local/Synocom/GiveIt/Helper/Data.php
+++ src/app/code/local/Synocom/GiveIt/Helper/Data.php
@@ -1,4 +1,15 @@
 <?php
-class Synocom_GiveIt_Helper_Data extends Mage_Core_Helper_Data
+
+/**
+ * GiveIt extension
+ *
+ * @category   Synocom
+ * @package    Synocom_GiveIt
+ * @copyright  Copyright (c) 2013 Synocom BV (http://www.synocom.nl)
+ * @author     Brennon Blokland <info@synocom.nl>
+ */
+class Synocom_GiveIt_Helper_Data
+    extends Mage_Core_Helper_Data
 {
+
 }
