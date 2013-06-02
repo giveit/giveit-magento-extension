dc802fc60e68a51e79dc8f9140e35f10c46c354d
Brennon Blokland <b.blokland@drecomm.nl>
2013-06-03 01:38:41 +0200

    [TASK] Add phpDoc

--- src/app/code/local/Synocom/GiveIt/Block/Button.php
+++ src/app/code/local/Synocom/GiveIt/Block/Button.php
@@ -1,30 +1,83 @@
 <?php
 
+/**
+ * GiveIt extension
+ *
+ * @category   Synocom
+ * @package    Synocom_GiveIt
+ * @copyright  Copyright (c) 2013 Synocom BV (http://www.synocom.nl)
+ * @author     Brennon Blokland <info@synocom.nl>
+ */
 class Synocom_GiveIt_Block_Button
     extends Mage_Core_Block_Template
 {
 
-    /**
-     * Factory method loads the GiveIt SDK
-     */
     protected function _construct()
     {
-        $this->setSdk(Mage::getSingleton('synocom_giveit/giveIt'));
+        parent::_construct();
+        $this->_getSdk();
+    }
+    /**
+     * Get give it SDK
+     */
+    protected function _getSdk()
+    {
+        return Mage::getSingleton('synocom_giveit/giveIt');
     }
 
+    /**
+     * Render block HTML
+     *
+     * @return string
+     */
     protected function _toHtml()
     {
+        $html = '';
+
         $this->_setProductDetails();
         $this->_addDeliveryOptions();
 
         if ($this->_validate()) {
-            echo $this->getSdkProduct()->getButtonHTML();
-            $this->getSdk()->outputButtonJS();
+            $html = $this->_renderButton();
+        }
+
+        return $html;
+    }
+
+    /**
+     * Render button
+     *
+     * @return string
+     */
+    protected function _renderButton()
+    {
+        $html = '';
+
+        $buttonHtml = $this->_getSdkProduct()->getButtonHTML();
+        $buttonJs   = $this->outputButtonJS();
+        $html       = $buttonHtml . $buttonJs;
+
+        return $html;
+    }
+
+    /**
+     * This function outputs the JS inclusion for the button
+     *
+     * @return boolean
+     */
+    public function outputButtonJS()
+    {
+        $sdk = $this->_getSdk();
+        if (!$sdk->jsOutput) {
+            $sdk->jsOutput = true;
+            return $sdk->getButtonJS();
         }
     }
 
     /**
-     * Get current product
+     * Get current product used in product info
+     *
+     * @return Mage_Catalog_Model_Product
      */
     public function getProduct()
     {
@@ -57,27 +110,23 @@ class Synocom_GiveIt_Block_Button
         $product = $this->getProduct();
         /* @var $product Mage_Catalog_Model_Product */
 
-        if (!$product) {
-            return false;
-        }
-
-
         if ($product->getId()) {
             $code  = $product->getSku();
             $price = (int) round($product->getFinalPrice() * 100, 0);
             $name  = $product->getName();
             $image = $product->getImageUrl();
 
-            $this->getSdkProduct()->setProductDetails($code, $price, $name, $image);
+            $this->_getSdkProduct()->setProductDetails($code, $price, $name, $image);
         }
+
     }
 
     /**
      * Get an instance of the Give it SDK product object
      *
-     * @return type
+     * @return \GiveIt\SDK\Product
      */
-    protected function getSdkProduct()
+    protected function _getSdkProduct()
     {
         if (!$this->hasSdkProduct()) {
             $this->setSdkProduct(new \GiveIt\SDK\Product());
@@ -87,21 +136,28 @@ class Synocom_GiveIt_Block_Button
 
     /**
      * Add delivery options to product
+     *
+     * @return void
      */
     protected function _addDeliveryOptions()
     {
-        $sdkProduct = $this->getSdkProduct();
+        $sdkProduct = $this->_getSdkProduct();
 
         $delivery = $sdkProduct->addDeliveryOption('delivery', 'Delivery Option');
         $sdkProduct->addChoice($delivery, 'europe', 'Europe', 900);
         $sdkProduct->addChoice($delivery, 'usa', 'United States', 499);
     }
 
+    /**
+     * Validate the sdk product
+     *
+     * @return boolean
+     */
     protected function _validate()
     {
-        $result = $this->getSdkProduct()->validate();
+        $result = $this->_getSdkProduct()->validate();
         if (!$result) {
-            Mage::log($this->getSdkProduct()->getErrorsHTML());
+            Mage::log($this->_getSdkProduct()->getErrorsHTML());
         }
         return $result;
     }
