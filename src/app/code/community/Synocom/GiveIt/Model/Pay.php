89a22e9cc654d5c844e08c0f4fde5d68efcc265b
Szymon Niedziela <sniedziela@light4website.com>
2013-09-03 13:43:46 +0200

    [G2] Give it payment method

---
+++ src/app/code/community/Synocom/GiveIt/Model/Pay.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * GiveIt extension
+ *
+ * @category   Synocom
+ * @package    Synocom_GiveIt
+ * @copyright  Copyright (c) 2013 Light4website (http://www.light4website.com)
+ * @author     Szymon Niedziela <office@light4website.com>
+ *
+ */
+
+class Synocom_GiveIt_Model_Pay extends Mage_Payment_Model_Method_Abstract {
+
+    protected $_code = 'giveit_payment';
+
+}
